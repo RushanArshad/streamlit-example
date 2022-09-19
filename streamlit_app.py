650672f61e3b682c390fbb5de484a64b61a166f0
Rushan Arshad <rushanarshad1@gmail.com>
2022-09-19 22:40:20 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,28 +11,29 @@ data_till = pd.read_excel('Data Summary.xlsx', sheet_name = 'Data-Till Activity 
 
 data_front = data_front.astype(str)
 
-st.dataframe(data_front)
+if st.button("Populate Data")
+    st.dataframe(data_front)
 
+if st.button("Populate Grid Data")
 
+    gb = GridOptionsBuilder.from_dataframe(data_front)
+    gb.configure_pagination(paginationAutoPageSize=True) #Add pagination
+    gb.configure_side_bar() #Add a sidebar
+    gb.configure_selection('multiple', use_checkbox=True, groupSelectsChildren="Group checkbox select children") #Enable multi-row selection
+    gridOptions = gb.build()
 
-gb = GridOptionsBuilder.from_dataframe(data_front)
-gb.configure_pagination(paginationAutoPageSize=True) #Add pagination
-gb.configure_side_bar() #Add a sidebar
-gb.configure_selection('multiple', use_checkbox=True, groupSelectsChildren="Group checkbox select children") #Enable multi-row selection
-gridOptions = gb.build()
-
-grid_response = AgGrid(
-    data_front,
-    gridOptions=gridOptions,
-    data_return_mode='AS_INPUT', 
-    update_mode='MODEL_CHANGED', 
-    fit_columns_on_grid_load=False,
-    theme="streamlit", #Add theme color to the table
-    enable_enterprise_modules=True,
-    height=350, 
-    width='100%',
-    reload_data=True
-)
+    grid_response = AgGrid(
+        data_front,
+        gridOptions=gridOptions,
+        data_return_mode='AS_INPUT', 
+        update_mode='MODEL_CHANGED', 
+        fit_columns_on_grid_load=False,
+        theme="streamlit", #Add theme color to the table
+        enable_enterprise_modules=True,
+        height=350, 
+        width='100%',
+        reload_data=True
+    )
 
 data_front = grid_response['data']
 selected = grid_response['selected_rows'] 
